d4cbd43d74b990c76d8f41eaa0d83a81cc7dcfa0
Hao Ling (haoling) <haoling@cisco.com>
2021-08-30 12:49:57 +0800

    share online link

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
+
+var SHARE_LINK = null;
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const sharinglink = urlParams.get('sharinglink');
+if (typeof sharinglink === 'undefined' || sharinglink === null) 
+{
+    SHARE_LINK = sharinglink;
+}
+
+
 (function initialize(){
     app = new window.Webex.Application();
     app.onReady().then(() => 
     {
         log('onReady()', {message:'app is ready'});
+        if (typeof SHARE_LINK === 'undefined' || SHARE_LINK === null) 
+        {
+            showHostView();
+        }
+        else
+        {
+            showAttendeeView();
+        }
+
         app.listen().then(function () {
             app.on("application:shareStateChanged", function (isShared) {
             });
@@ -12,6 +33,45 @@
     });
         
 })();
+
+function showHostView()
+{
+    //document.getElementById("sharinglink").hidden = false;
+    //document.getElementById("sharingbutton").hidden = false;
+    document.getElementById("sharinglink").style.display = "none";
+    document.getElementById("sharingbutton").style.display = "none";
+}
+
+function showAttendeeView()
+{
+    //document.getElementById("sharinglink").hidden = true;
+    //document.getElementById("sharingbutton").hidden = true;
+    document.getElementById("sharinglink").style.display = "block";
+    document.getElementById("sharingbutton").style.display = "block";
+}
+
+window.onkeyup = keyup;
+
+function keyup(e) {
+    //setting your input text to the global Javascript Variable for every key press
+    SHARE_LINK = e.target.value;
+}
+
+function setShareUrl() 
+  {
+    var internalUrl = "https://comingzero.github.io/onlinedocumentsharing/?sharinglink=" + SHARE_LINK;
+    var externalUrl = internalUrl;
+    var title = "Countdown Timer";
+    var opt = {};
+
+    app.setShareUrl(internalUrl, externalUrl, title, opt)
+      .then(function (res) {
+        log("Promise setShareUrl success", JSON.stringify(res));
+      })
+      .catch(function (reason) {
+        log("setShareUrl: fail reason=" + reason);
+      });
+  }
 
 function log(type, data) {
     /*
